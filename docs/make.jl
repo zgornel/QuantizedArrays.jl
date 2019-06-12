26b2afc25813e4b220e5a4eef8db40b24d1b54aa
Corneliu Cofaru <cornel@oxoaresearch.com>
2019-06-12 10:47:22 +0200

    Added tests dependecy; minor docgen changes

--- docs/make.jl
+++ docs/make.jl
@@ -8,7 +8,7 @@ push!(LOAD_PATH,"../src/")
 # Make documentation
 makedocs(
     modules = [QuantizedArrays],
-    format = :html,
+    format = Documenter.HTML(),
     sitename = "  ",
     authors = "Corneliu Cofaru, 0x0α Research",
     clean = true,
